331ca2882776e22a46fc4417daddfbf62d47a366
clintar <clint@gurutechnologies.net>
2015-12-02 18:42:05 -0700

    renamed website to not conflict

---
+++ website-sample/config.js
@@ -0,0 +1,17 @@
+var api = "http://poolhost.com:1117";
+
+var poolHost = "poolhost.com";
+
+var irc = "irc.freenode.net/#poolhost";
+
+var email = "support@poolhost.com";
+
+var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+var themeCss = "themes/default-theme.css";
